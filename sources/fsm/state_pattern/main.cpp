558242b4020a5d1dee4bded93b80ad778d8cff67
vss <dizel3d@gmail.com>
2011-02-03 13:43:31 +0300

    State pattern

--- sources/fsm/state_pattern/main.cpp
+++ sources/fsm/state_pattern/main.cpp
@@ -5,79 +5,86 @@ using namespace std;
 
 class Event { private: virtual void _use_rtti() {} };
 
-template <class Automaton> class CommonState {
-public:
-	typedef CommonState (Automaton::*Handler)(const Event&);
+template <class A> class Automaton {
+	public:
+		void dispatch(const Event& e) {
+			state = state.invoke(static_cast<A*>(this), e);
+		}
 
-	CommonState(const Handler& s) : dispatch(s) { }
+	protected:
+		class State {
+			public:
+				typedef State (A::*Handler)(const Event&);
 
-	CommonState invoke(Automaton* au, const Event& e) const {
-		return (au->*dispatch)(e);
-	}
+				State(const Handler& s) : dispatch(s) { }
+
+				State invoke(A* au, const Event& e) const {
+					return (au->*dispatch)(e);
+				}
 		
-private:
-	Handler dispatch;
+			private:
+				Handler dispatch;
+		};
+ 
+		Automaton(const State& s) : state(s) { }
+
+	private:
+		State state;
 };
 
 class Event1 : public Event { };
 class Event2 : public Event { };
 
-class Fsm {
-public:
-	Fsm() : state(&Fsm::A) { }
+class Fsm : public Automaton<Fsm> {
+	public:
+		Fsm() : Automaton<Fsm>(State(&Fsm::A)) { }
 
-	void dispatch(const Event& e) {
-		cout << "State ";
-		state = state.invoke(this, e);
-		cout << ". Event " << typeid(e).name() << endl;
-	}
+	private:
+		State A(const Event& e) {
+			printState("A", e);
+			if (typeid(e) == typeid(Event1)) {
+				/* do_A_onEvent1() */
+				return State(&Fsm::B);
+			}
+			else if (typeid(e) == typeid(Event2)) {
+				/* do_A_onEvent2() */
+			}
+			return State(&Fsm::A);
+		}
 
-private:
-	typedef CommonState<Fsm> State;
-
-	State state;
-
-	State A(const Event& e) {
-		cout << "A";
-		if (typeid(e) == typeid(Event1)) {
-			/* do_A_onEvent1() */
+		State B(const Event &e) {
+			printState("B", e);
+			if (typeid(e) == typeid(Event2)) {
+				if (guard_B_onEvent2()) {
+					/* do_B_onEvent2() */
+					return State(&Fsm::A);
+				}
+				else {
+					return State(&Fsm::C);
+				}
+			}
 			return State(&Fsm::B);
 		}
-		else if (typeid(e) == typeid(Event2)) {
-			/* do_A_onEvent2() */
+
+		State C(const Event &e) {
+			printState("C", e);
+			/* ... */
+			return State(&Fsm::C);
 		}
-		return State(&Fsm::A);
-	}
 
-	State B(const Event &e) {
-		cout << "B";
-		if (typeid(e) == typeid(Event2)) {
-			if (guard_B_onEvent2()) {
-				/* do_B_onEvent2() */
-				return State(&Fsm::A);
-			}
-			else {
-				return State(&Fsm::C);
-			}
+		State D(const Event &e) {
+			printState("D", e);
+			/* ... */
+			return State(&Fsm::D);
 		}
-		return State(&Fsm::B);
-	}
 
-	State C(const Event &e) {
-		cout << "C";
-		/* ... */
-		return State(&Fsm::C);
-	}
+		bool guard_B_onEvent2() {
+			return true;
+		}
 
-	State D(const Event &e) {
-		cout << "D";
-		/* ... */
-		return State(&Fsm::D);
-	}
-
-	bool guard_B_onEvent2() {
-		return true;
-	}
+		void printState(const char* title, const Event& e) {
+			cout << "State " << title << ". Event " << typeid(e).name() << endl;
+		}
 };
 
 int main() {
